9348add3946d6376d5976ca4cfcadb8b370da884
Lena Brueder <lbruder@rim.com>
2012-07-02 16:13:59 +0200

    added some things about chord estimation

--- src/music/feature_extraction/chords.hpp
+++ src/music/feature_extraction/chords.hpp
@@ -3,27 +3,31 @@
 
 #include <string>
 
-//Holds one chord, e.g. c or A or D7.
-//add some similarity measure (zwei dur- oder zwei mollakkorde vergleichen, ...)
-class Chord
+namespace music
 {
-private:
-    bool note[12];  //if a note is within this chord, or not
-protected:
-    
-public:
-    std::string getChordAsString();
-    bool hasNote(int note);
-};
+    //Holds one chord, e.g. c or A or D7.
+    //add some similarity measure (zwei dur- oder zwei mollakkorde vergleichen, ...)
+    class Chord
+    {
+    private:
+        bool note[12];  //if a note is within this chord, or not
+    protected:
+        
+    public:
+        std::string getChordAsString();
+        bool hasNote(int note);
+    };
 
-class ChordEstimator
-{
-private:
-    
-protected:
-    
-public:
-    
-};
-
+    class ChordEstimator
+    {
+    private:
+        ConstantQTransformResult* transformResult;
+        double timeResolution;
+    protected:
+        
+    public:
+        ChordEstimator(ConstantQTransformResult* transformResult, double timeResolution = 0.005);
+        Chord* estimateChord(double fromTime, double toTime);
+    };
+}
 #endif  //CHORDS_HPP
