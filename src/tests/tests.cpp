48e671420c717b44fe8b0a4afb9018e1706b6cda
Lena Brueder <lbruder@rim.com>
2012-07-12 16:30:13 +0200

    chord estimation test now has more test cases
    timbre extimation tests started

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -474,25 +474,25 @@ namespace tests
         std::queue<double> minBPMs;
         std::queue<double> maxBPMs;
         
-        files.push("testdata/metronom-80.mp3");    //80bpm
+        files.push("testdata/rhythm-metronom-80.mp3");    //80bpm
         minBPMs.push(75);
         maxBPMs.push(85);
-        files.push("testdata/metronom-180.mp3");    //180bpm
+        files.push("testdata/rhythm-metronom-180.mp3");    //180bpm
         minBPMs.push(175);
         maxBPMs.push(185);
-        files.push("testdata/drums-80-4_4-8th_hihat.mp3");    //80bpm
+        files.push("testdata/rhythm-drums-80-4_4-8th_hihat.mp3");    //80bpm
         minBPMs.push(75);
         maxBPMs.push(85);
-        files.push("testdata/drums-100-4_4-8th_hihat.mp3");    //100bpm
+        files.push("testdata/rhythm-drums-100-4_4-8th_hihat.mp3");    //100bpm
         minBPMs.push(95);
         maxBPMs.push(105);
-        files.push("testdata/drums-180-4_4-8th_hihat.mp3");    //180bpm
+        files.push("testdata/rhythm-drums-180-4_4-8th_hihat.mp3");    //180bpm
         minBPMs.push(175);
         maxBPMs.push(185);
-        files.push("testdata/drums-200-4_4-8th_hihat.mp3");    //200bpm
+        files.push("testdata/rhythm-drums-200-4_4-8th_hihat.mp3");    //200bpm
         minBPMs.push(195);
         maxBPMs.push(205);
-        files.push("testdata/drums-80-4_4-16th_hihat.mp3");    //80bpm
+        files.push("testdata/rhythm-drums-80-4_4-16th_hihat.mp3");    //80bpm
         minBPMs.push(75);
         maxBPMs.push(85);
         
@@ -578,7 +578,7 @@ namespace tests
         delete cqt;
         delete lowpassFilter;
         
-        return EXIT_FAILURE;
+        return EXIT_SUCCESS;
     }
     
     /**
@@ -764,79 +764,79 @@ namespace tests
         std::queue<std::string> files;
         std::queue<std::string> chords;
         
-        files <<  "./testdata/major-c-guitar.mp3";
-        chords << "major C";
-        files <<  "./testdata/major-c-keyboard.mp3";
-        chords << "major C";
-        files <<  "./testdata/major-c#-keyboard.mp3";
-        chords << "major C#";
-        files <<  "./testdata/major-d-guitar.mp3";
-        chords << "major D";
-        files <<  "./testdata/major-d-keyboard.mp3";
-        chords << "major D";
-        files <<  "./testdata/major-d#-keyboard.mp3";
-        chords << "major D#";
-        files <<  "./testdata/major-e-guitar.mp3";
-        chords << "major E";
-        files <<  "./testdata/major-e-keyboard.mp3";
-        chords << "major E";
-        files <<  "./testdata/major-f-guitar.mp3";
-        chords << "major F";
-        files <<  "./testdata/major-f-keyboard.mp3";
-        chords << "major F";
-        files <<  "./testdata/major-f#-keyboard.mp3";
-        chords << "major F#";
-        files <<  "./testdata/major-g-guitar.mp3";
-        chords << "major G";
-        files <<  "./testdata/major-g-keyboard.mp3";
-        chords << "major G";
-        files <<  "./testdata/major-g#-keyboard.mp3";
-        chords << "major G#";
-        files <<  "./testdata/major-a-guitar.mp3";
-        chords << "major A";
-        files <<  "./testdata/major-a-keyboard.mp3";
-        chords << "major A";
-        files <<  "./testdata/major-a#-keyboard.mp3";
-        chords << "major A#";
-        files <<  "./testdata/major-b-keyboard.mp3";
-        chords << "major B";
+        files <<  "./testdata/chord-major-c-guitar.mp3";
+        chords << "C";
+        files <<  "./testdata/chord-major-c-keyboard.mp3";
+        chords << "C";
+        files <<  "./testdata/chord-major-c#-keyboard.mp3";
+        chords << "C#";
+        files <<  "./testdata/chord-major-d-guitar.mp3";
+        chords << "D";
+        files <<  "./testdata/chord-major-d-keyboard.mp3";
+        chords << "D";
+        files <<  "./testdata/chord-major-d#-keyboard.mp3";
+        chords << "D#";
+        files <<  "./testdata/chord-major-e-guitar.mp3";
+        chords << "E";
+        files <<  "./testdata/chord-major-e-keyboard.mp3";
+        chords << "E";
+        files <<  "./testdata/chord-major-f-guitar.mp3";
+        chords << "F";
+        files <<  "./testdata/chord-major-f-keyboard.mp3";
+        chords << "F";
+        files <<  "./testdata/chord-major-f#-keyboard.mp3";
+        chords << "F#";
+        files <<  "./testdata/chord-major-g-guitar.mp3";
+        chords << "G";
+        files <<  "./testdata/chord-major-g-keyboard.mp3";
+        chords << "G";
+        files <<  "./testdata/chord-major-g#-keyboard.mp3";
+        chords << "G#";
+        files <<  "./testdata/chord-major-a-guitar.mp3";
+        chords << "A";
+        files <<  "./testdata/chord-major-a-keyboard.mp3";
+        chords << "A";
+        files <<  "./testdata/chord-major-a#-keyboard.mp3";
+        chords << "A#";
+        files <<  "./testdata/chord-major-b-keyboard.mp3";
+        chords << "B";
         
-        files <<  "./testdata/minor-c-guitar.mp3";
-        chords << "minor C";
-        files <<  "./testdata/minor-c-keyboard.mp3";
-        chords << "minor C";
-        files <<  "./testdata/minor-c#-keyboard.mp3";
-        chords << "minor C#";
-        files <<  "./testdata/minor-d-guitar.mp3";
-        chords << "minor D";
-        files <<  "./testdata/minor-d-keyboard.mp3";
-        chords << "minor D";
-        files <<  "./testdata/minor-d#-keyboard.mp3";
-        chords << "minor D#";
-        files <<  "./testdata/minor-e-guitar.mp3";
-        chords << "minor E";
-        files <<  "./testdata/minor-e-keyboard.mp3";
-        chords << "minor E";
-        files <<  "./testdata/minor-f-guitar.mp3";
-        chords << "minor F";
-        files <<  "./testdata/minor-f-keyboard.mp3";
-        chords << "minor F";
-        files <<  "./testdata/minor-f#-keyboard.mp3";
-        chords << "minor F#";
-        files <<  "./testdata/minor-g-guitar.mp3";
-        chords << "minor G";
-        files <<  "./testdata/minor-g#-keyboard.mp3";
-        chords << "minor G#";
-        files <<  "./testdata/minor-a-guitar.mp3";
-        chords << "minor A";
-        files <<  "./testdata/minor-a-keyboard.mp3";
-        chords << "minor A";
-        files <<  "./testdata/minor-a#-keyboard.mp3";
-        chords << "minor A#";
-        files <<  "./testdata/minor-b-guitar.mp3";
-        chords << "minor B";
-        files <<  "./testdata/minor-b-keyboard.mp3";
-        chords << "minor B";
+        files <<  "./testdata/chord-minor-c-guitar.mp3";
+        chords << "c";
+        files <<  "./testdata/chord-minor-c-keyboard.mp3";
+        chords << "c";
+        files <<  "./testdata/chord-minor-c#-keyboard.mp3";
+        chords << "c#";
+        files <<  "./testdata/chord-minor-d-guitar.mp3";
+        chords << "d";
+        files <<  "./testdata/chord-minor-d-keyboard.mp3";
+        chords << "d";
+        files <<  "./testdata/chord-minor-d#-keyboard.mp3";
+        chords << "d#";
+        files <<  "./testdata/chord-minor-e-guitar.mp3";
+        chords << "e";
+        files <<  "./testdata/chord-minor-e-keyboard.mp3";
+        chords << "e";
+        files <<  "./testdata/chord-minor-f-guitar.mp3";
+        chords << "f";
+        files <<  "./testdata/chord-minor-f-keyboard.mp3";
+        chords << "f";
+        files <<  "./testdata/chord-minor-f#-keyboard.mp3";
+        chords << "f#";
+        files <<  "./testdata/chord-minor-g-guitar.mp3";
+        chords << "g";
+        files <<  "./testdata/chord-minor-g#-keyboard.mp3";
+        chords << "g#";
+        files <<  "./testdata/chord-minor-a-guitar.mp3";
+        chords << "a";
+        files <<  "./testdata/chord-minor-a-keyboard.mp3";
+        chords << "a";
+        files <<  "./testdata/chord-minor-a#-keyboard.mp3";
+        chords << "a#";
+        files <<  "./testdata/chord-minor-b-guitar.mp3";
+        chords << "b";
+        files <<  "./testdata/chord-minor-b-keyboard.mp3";
+        chords << "b";
 
         while (!files.empty())
         {
@@ -885,6 +885,75 @@ namespace tests
                 //chordstr << *chord << ConsoleColors::defaultText() << std::endl << std::flush;
             //}
         }
+        
+        std::queue<std::pair<std::string, std::vector<std::pair<double, std::string> > > > data;
+        std::vector<std::pair<double, std::string> > times;
+        
+        times.push_back(std::pair<double, std::string>( 1.2, "G"));
+        times.push_back(std::pair<double, std::string>( 3.5, "e"));
+        times.push_back(std::pair<double, std::string>( 6.0, "C"));
+        times.push_back(std::pair<double, std::string>( 7.6, "D"));
+        times.push_back(std::pair<double, std::string>( 9.7+1.2, "G"));
+        times.push_back(std::pair<double, std::string>( 9.7+3.5, "e"));
+        times.push_back(std::pair<double, std::string>( 9.7+6.0, "C"));
+        times.push_back(std::pair<double, std::string>( 9.7+7.6, "D"));
+        times.push_back(std::pair<double, std::string>( 19.3+1.2, "G"));
+        times.push_back(std::pair<double, std::string>( 19.3+3.5, "e"));
+        times.push_back(std::pair<double, std::string>( 19.3+6.0, "C"));
+        times.push_back(std::pair<double, std::string>( 19.3+7.6, "D"));
+        
+        data.push(std::pair<std::string, std::vector<std::pair<double, std::string> > >(std::string("./testdata/mixture-bass_and_rhgit.mp3"), times));
+        data.push(std::pair<std::string, std::vector<std::pair<double, std::string> > >(std::string("./testdata/mixture-drums_and_rhgit.mp3"), times));
+        data.push(std::pair<std::string, std::vector<std::pair<double, std::string> > >(std::string("./testdata/mixture-all_but_sologit.mp3"), times));
+        data.push(std::pair<std::string, std::vector<std::pair<double, std::string> > >(std::string("./testdata/mixture-all_but_drums.mp3"), times));
+        data.push(std::pair<std::string, std::vector<std::pair<double, std::string> > >(std::string("./testdata/mixture-all.mp3"), times));
+        
+        //these files fail.
+        //data.push(std::pair<std::string, std::vector<std::pair<double, std::string> > >(std::string("./testdata/mixture-all_but_bass.mp3"), times));
+        //data.push(std::pair<std::string, std::vector<std::pair<double, std::string> > >(std::string("./testdata/mixture-bass_and_sologit.mp3"), times));
+        
+        while (!data.empty())
+        {
+            std::string filename = data.front().first;
+            times = data.front().second;
+            DEBUG_OUT("using file \"" << filename << "\"", 10);
+            data.pop();
+            
+            musicaccess::SoundFile file;
+            CHECK(!file.isFileOpen());
+            CHECK(file.open(filename, true));
+            CHECK(file.isFileOpen());
+            
+            float* buffer = NULL;
+            buffer = new float[file.getSampleCount()];
+            CHECK(buffer != NULL);
+            
+            int sampleCount = file.readSamples(buffer, file.getSampleCount());
+            //estimated size might not be accurate!
+            CHECK_OP(sampleCount, >=, 0.9*file.getSampleCount());
+            CHECK_OP(sampleCount, <=, 1.1*file.getSampleCount());
+            
+            musicaccess::Resampler22kHzMono resampler;
+            //int sampleCount = file.getSampleCount();
+            DEBUG_OUT("resampling input file...", 15);
+            resampler.resample(file.getSampleRate(), &buffer, sampleCount, file.getChannelCount());
+            
+            CHECK_OP(sampleCount, <, file.getSampleCount());
+            
+            DEBUG_OUT("applying constant q transform...", 15);
+            music::ConstantQTransformResult* transformResult = cqt->apply(buffer, sampleCount);
+            CHECK(transformResult != NULL);
+            
+            music::ChordEstimator chordEstimator(transformResult, 0.05);
+            for (std::vector<std::pair<double, std::string> >::iterator it = times.begin(); it != times.end(); it++)
+            {
+                music::ChordHypothesis* chordHypothesis = chordEstimator.estimateChord(it->first-0.1, it->first+0.1);
+                
+                DEBUG_OUT("at time " << it->first << " we should find chord \"" << it->second << "\"", 25);
+                CHECK_EQ(it->second, chordHypothesis->getMaxHypothesisAsString());
+            }
+        }
+        
         return EXIT_SUCCESS;
     }
     
@@ -909,8 +978,7 @@ namespace tests
         
         musicaccess::SoundFile file;
         CHECK(!file.isFileOpen());
-        //CHECK(file.open("./testdata/test.mp3", true));
-        CHECK(file.open("./goodbye.mp3", true));
+        CHECK(file.open("./testdata/instrument-alto_sax.mp3", true));
         CHECK(file.isFileOpen());
         
         float* buffer = NULL;
@@ -933,16 +1001,98 @@ namespace tests
         music::ConstantQTransformResult* transformResult = cqt->apply(buffer, sampleCount);
         CHECK(transformResult != NULL);
         
-        std::ofstream chordstr("chords.dat");
+        Eigen::VectorXd timbreChroma(15);
+        //overtones is a vector filled with info about how to add overtones
+        //first element is the lower index of the two bins containing the overtone
+        //if only one bin is used, the second parameter is one and the following is 0.0.
+        //the sum of parameter 2 and 3 is always 1.0.
+        //if two bins are used for an overtone, put the percentage in parameters 2 and 3.
+        std::vector<std::pair<int, std::pair<double, double> > > overtones;
+        overtones.push_back(std::pair<int, std::pair<double, double> >( 0, std::pair<double, double>(1.00, 0.00)));
+        overtones.push_back(std::pair<int, std::pair<double, double> >(12, std::pair<double, double>(1.00, 0.00)));
+        overtones.push_back(std::pair<int, std::pair<double, double> >(19, std::pair<double, double>(0.98, 0.02)));
+        overtones.push_back(std::pair<int, std::pair<double, double> >(24, std::pair<double, double>(1.00, 0.00)));
+        overtones.push_back(std::pair<int, std::pair<double, double> >(28, std::pair<double, double>(0.05, 0.95)));
+        overtones.push_back(std::pair<int, std::pair<double, double> >(31, std::pair<double, double>(0.98, 0.02)));
+        overtones.push_back(std::pair<int, std::pair<double, double> >(33, std::pair<double, double>(0.40, 0.60)));
+        overtones.push_back(std::pair<int, std::pair<double, double> >(36, std::pair<double, double>(1.00, 0.00)));
+        overtones.push_back(std::pair<int, std::pair<double, double> >(38, std::pair<double, double>(0.99, 0.01)));
+        overtones.push_back(std::pair<int, std::pair<double, double> >(39, std::pair<double, double>(0.05, 0.95)));
+        overtones.push_back(std::pair<int, std::pair<double, double> >(41, std::pair<double, double>(0.55, 0.45)));
         
-        music::ChordEstimator chordEstimator(transformResult, 0.05);
-        for (double time=0.0; time<transformResult->getOriginalDuration(); time += 0.5)
+        std::ofstream timbredata("timbredata.dat", std::ios_base::app);
+        
+        int binCount = transformResult->getBinsPerOctave()*transformResult->getOctaveCount();
+        Eigen::VectorXd vec(binCount);
+        Eigen::VectorXd timbre(11);
+        std::vector<std::pair<double, int> > sortVec(binCount);
+        double time;
+        //for (double time=0.0; time<transformResult->getOriginalDuration(); time += 0.125/8)
+        for (int i=0; i<transformResult->getOriginalDuration() * 32; i++)
         {
-            music::ChordHypothesis* chord = chordEstimator.estimateChord(time, time + 0.5);
-        
-            DEBUG_OUT(ConsoleColors::yellow() << "chord at time " << time << ":" << std::endl << ConsoleColors::defaultText() << *chord, 10);
-            chordstr << *chord << ConsoleColors::defaultText() << std::endl << std::flush;
+            time = (1.0/32.0) * i;
+            
+            for (int octave=0; octave < transformResult->getOctaveCount(); octave++)
+            {
+                for (int bin=0; bin < transformResult->getBinsPerOctave(); bin++)
+                {
+                    int pos = octave*transformResult->getBinsPerOctave() + bin;
+                    vec[pos] = (std::abs(transformResult->getNoteValueNoInterpolation(time, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.010, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.020, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.030, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.040, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.050, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.060, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.070, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.080, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.090, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.100, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.110, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.120, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.130, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.140, octave, bin)) +
+                            std::abs(transformResult->getNoteValueNoInterpolation(time-0.150, octave, bin)))
+                            /16.0;
+                    sortVec[pos] = std::pair<double, int>(vec[pos], pos);
+                }
+            }
+            std::sort(sortVec.begin(), sortVec.end());
+            if (i%8)
+            {
+                for (int otone=0; otone<11; otone++)
+                {
+                    timbre[otone] = 0.0;
+                }
+            }
+            //add loudest x parts for timbre
+            for (int j=0; j<1; j++)
+            {
+                int loudestPos = sortVec[binCount-j-1].second;
+                for (int otone=0; otone<11; otone++)
+                {
+                    int otonePos = loudestPos + overtones[otone].first;
+                    if (otonePos > binCount-2)
+                        continue;
+                    timbre[otone] += vec[otonePos] * overtones[otone].second.first + 
+                        vec[otonePos+1] * overtones[otone].second.second;
+                }
+            }
+            
+            if ((i+1)%8)
+            {
+                timbre.normalize();
+            
+                if ((time >= 1.0 && time <= 1.0+8*0.125) /*|| (time >= 6.2 && time <= 6.2+0.125)*/)
+                {
+                    DEBUG_OUT("timbre at time " << time << ": " << std::endl << timbre, 10);
+                    for (int k=0; k<11; k++)
+                        timbredata << timbre[k] << " ";
+                    timbredata << std::endl;
+                }
+            }
         }
+        timbredata << std::endl;
         
         return EXIT_SUCCESS;
     }
