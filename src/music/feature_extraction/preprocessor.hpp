fbc923e29307288d86251d55f16cc4d7e9f93d1b
Lena Brueder <lbruder@rim.com>
2012-08-13 12:10:35 +0200

    added a progress callback object that helps give the user feedback about the programs state.

---
+++ src/music/feature_extraction/preprocessor.hpp
@@ -0,0 +1,16 @@
+#ifndef PREPROCESSOR_HPP
+#define PREPROCESSOR_HPP
+
+#include <string>
+
+class FilePreprocessor
+{
+private:
+    
+protected:
+    
+public:
+    bool preprocessFile(std::string filename);
+};
+
+#endif //PREPROCESSOR_HPP
