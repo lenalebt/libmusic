18c8f740ce646c5afa5f98c7be3aae02c4dd99b2
lbruder <lbruder@0c85f470-1217-11de-aba5-9925624bc3b7>
2012-11-09 15:16:25 +0000

    added minEnergy variable to timbre vector estimator
    git-svn-id: file:///mnt/svn/libmusic/trunk@1005 0c85f470-1217-11de-aba5-9925624bc3b7

--- src/music/feature_extraction/timbre.hpp
+++ src/music/feature_extraction/timbre.hpp
@@ -37,13 +37,14 @@ namespace music
         DCT dct;
         unsigned int timbreVectorSize;
         Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> cosValues;
+        float minEnergy;
         
         Eigen::Matrix<kiss_fft_scalar, Eigen::Dynamic, 1> estimateTimbre1(double fromTime, double toTime);
         Eigen::Matrix<kiss_fft_scalar, Eigen::Dynamic, 1> estimateTimbre2(double fromTime, double toTime);
         Eigen::Matrix<kiss_fft_scalar, Eigen::Dynamic, 1> estimateTimbre3(double fromTime, double toTime);
         Eigen::Matrix<kiss_fft_scalar, Eigen::Dynamic, 1> estimateTimbre4(double fromTime, double toTime);
     public:
-        TimbreEstimator(ConstantQTransformResult* transformResult, unsigned int timbreVectorSize=12);
+        TimbreEstimator(ConstantQTransformResult* transformResult, unsigned int timbreVectorSize=12, float minEnergy=5.0);
         Eigen::Matrix<kiss_fft_scalar, Eigen::Dynamic, 1> estimateTimbre(double fromTime, double toTime);
     };
     
