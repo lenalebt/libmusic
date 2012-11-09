5aacc7387343c4303b139bd316880813791a9e9a
lbruder <lbruder@0c85f470-1217-11de-aba5-9925624bc3b7>
2012-11-09 15:03:08 +0000

    added classes for gaussian mixture models
    git-svn-id: file:///mnt/svn/libmusic/trunk@928 0c85f470-1217-11de-aba5-9925624bc3b7

---
+++ src/music/classification/gmm/gmm.cpp
@@ -0,0 +1,13 @@
+#include "gmm.hpp"
+
+namespace music
+{
+    double GaussianDiagCov::calculateValue(const Eigen::VectorXd& dataVector)
+    {
+        
+    }
+    double GaussianFullCov::calculateValue(const Eigen::VectorXd& dataVector)
+    {
+        
+    }
+}
