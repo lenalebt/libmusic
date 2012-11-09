4c71be13cbc8638033952f22593511fa514d284a
lbruder <lbruder@0c85f470-1217-11de-aba5-9925624bc3b7>
2012-11-09 14:52:22 +0000

    added some chords specific details
    git-svn-id: file:///mnt/svn/libmusic/trunk@833 0c85f470-1217-11de-aba5-9925624bc3b7

--- src/music/feature_extraction/chords.hpp
+++ src/music/feature_extraction/chords.hpp
@@ -1,4 +1,27 @@
 #ifndef CHORDS_HPP
 #define CHORDS_HPP
 
+//Holds one chord, e.g. c or A or D7.
+//add some similarity measure (zwei dur- oder zwei mollakkorde vergleichen, ...)
+class Chord
+{
+private:
+    bool note[12];  //if a note is within this chord, or not
+protected:
+    
+public:
+    std::string getChordAsString();
+    bool hasNote(int note);
+};
+
+class ChordEstimator
+{
+private:
+    
+protected:
+    
+public:
+    
+};
+
 #endif  //CHORDS_HPP
