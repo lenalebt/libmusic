c6793161b71aa6877f331a097feca0e30a56bd1a
lbruder <lbruder@0c85f470-1217-11de-aba5-9925624bc3b7>
2012-11-09 14:53:54 +0000

    changed calculation of mean/max/min/variance to use the order of the elements in memory to speed things up
    git-svn-id: file:///mnt/svn/libmusic/trunk@845 0c85f470-1217-11de-aba5-9925624bc3b7

--- src/music/feature_extraction/feature_extraction_helper.cpp
+++ src/music/feature_extraction/feature_extraction_helper.cpp
@@ -17,6 +17,110 @@ namespace music
     
     void PerBinStatistics::calculateMeanMinMax()
     {
+        int binsPerOctave = transformResult->getBinsPerOctave();
+        int octaveCount = transformResult->getOctaveCount();
+        
+        meanVector = new Eigen::VectorXd(octaveCount * binsPerOctave);
+        minVector = new Eigen::VectorXd(octaveCount * binsPerOctave);
+        maxVector = new Eigen::VectorXd(octaveCount * binsPerOctave);
+        
+        for (int octave=0; octave<octaveCount; octave++)
+        {
+            const Eigen::Matrix<std::complex<float>, Eigen::Dynamic, Eigen::Dynamic >* octaveMatrix = transformResult->getOctaveMatrix(octave);
+            
+            Eigen::VectorXd mean(binsPerOctave);
+            Eigen::VectorXd min(binsPerOctave);
+            Eigen::VectorXd max(binsPerOctave);
+            
+            for (int bin=0; bin<binsPerOctave; bin++)
+            {
+                mean[bin] = 0.0;
+                min[bin] = std::numeric_limits<double>::max();
+                max[bin] = -std::numeric_limits<double>::max();
+            }
+            
+            //matricies are column-major, so summing this way is faster than summing the other way round
+            for (int i=0; i<octaveMatrix->cols(); i++)
+            {
+                
+                for (int bin=0; bin<binsPerOctave; bin++)
+                {
+                    double val = std::abs((*octaveMatrix)(bin, i));
+                    
+                    mean[bin] += val;
+                    
+                    if (val < min[bin])
+                        min[bin] = val;
+                    
+                    if (val > max[bin])
+                        max[bin] = val;
+                }
+            }
+            
+            mean /= octaveMatrix->cols();
+            for (int bin=0; bin<binsPerOctave; bin++)
+            {
+                int pos = octave*binsPerOctave + bin;
+                (*meanVector)(pos) = mean[bin];
+                (*minVector)(pos) = min[bin];
+                (*maxVector)(pos) = max[bin];
+            }
+        }
+    }
+    
+    void PerBinStatistics::calculateVariance()
+    {
+        if (meanVector == NULL)
+            calculateMeanMinMax();
+        
+        int binsPerOctave = transformResult->getBinsPerOctave();
+        int octaveCount = transformResult->getOctaveCount();
+        
+        varianceVector = new Eigen::VectorXd(octaveCount * binsPerOctave);
+        for (int octave=0; octave<transformResult->getOctaveCount(); octave++)
+        {
+            const Eigen::Matrix<std::complex<float>, Eigen::Dynamic, Eigen::Dynamic >* octaveMatrix = transformResult->getOctaveMatrix(octave);
+            
+            Eigen::VectorXd variance(binsPerOctave);
+            
+            for (int bin=0; bin<binsPerOctave; bin++)
+                variance[bin] = 0.0;
+            
+            //matricies are column-major, so summing this way is faster than summing the other way round
+            for (int i=0; i<octaveMatrix->cols(); i++)
+            {
+                for (int bin=0; bin<binsPerOctave; bin++)
+                {
+                    double val = std::abs((*octaveMatrix)(bin, i)) - (*meanVector)[octave*binsPerOctave + bin];
+                    
+                    variance[bin] += val*val;
+                }
+            }
+            
+            variance /= octaveMatrix->cols();
+            for (int bin=0; bin<binsPerOctave; bin++)
+            {
+                int pos = octave*binsPerOctave + bin;
+                (*varianceVector)(pos) = variance[bin];
+            }
+        }
+    }
+    
+    PerTimeSliceStatistics::PerTimeSliceStatistics(ConstantQTransformResult* transformResult, double timeResolution) :
+        transformResult(transformResult),
+        meanVector(NULL),
+        varianceVector(NULL),
+        minVector(NULL),
+        maxVector(NULL),
+        
+        timeResolution(timeResolution)
+    {
+        assert(transformResult != NULL);
+        assert(timeResolution > 0.0);
+    }
+    
+    void PerTimeSliceStatistics::calculateMeanMinMax()
+    {
         meanVector = new Eigen::VectorXd(transformResult->getOctaveCount() * transformResult->getBinsPerOctave());
         minVector = new Eigen::VectorXd(transformResult->getOctaveCount() * transformResult->getBinsPerOctave());
         maxVector = new Eigen::VectorXd(transformResult->getOctaveCount() * transformResult->getBinsPerOctave());
@@ -51,7 +155,7 @@ namespace music
         }
     }
     
-    void PerBinStatistics::calculateVariance()
+    void PerTimeSliceStatistics::calculateVariance()
     {
         if (meanVector == NULL)
             calculateMeanMinMax();
--- src/music/feature_extraction/feature_extraction_helper.hpp
+++ src/music/feature_extraction/feature_extraction_helper.hpp
@@ -27,5 +27,29 @@ namespace music
         const Eigen::VectorXd* getMinVector() const             {return minVector;}
         const Eigen::VectorXd* getVarianceVector() const        {return varianceVector;}
     };
+    
+    class PerTimeSliceStatistics
+    {
+    private:
+        ConstantQTransformResult* transformResult;
+        
+    protected:
+        Eigen::VectorXd* meanVector;
+        Eigen::VectorXd* varianceVector;
+        Eigen::VectorXd* minVector;
+        Eigen::VectorXd* maxVector;
+        
+        double timeResolution;
+    public:
+        PerTimeSliceStatistics(ConstantQTransformResult* transformResult, double timeResolution);
+        
+        void calculateMeanMinMax();
+        void calculateVariance();
+        
+        const Eigen::VectorXd* getMeanVector() const            {return meanVector;}
+        const Eigen::VectorXd* getMaxVector() const             {return maxVector;}
+        const Eigen::VectorXd* getMinVector() const             {return minVector;}
+        const Eigen::VectorXd* getVarianceVector() const        {return varianceVector;}
+    };
 }
 #endif //FEATURE_EXTRACTION_HELPER_HPP
