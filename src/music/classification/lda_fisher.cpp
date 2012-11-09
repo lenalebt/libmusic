1d013fdfffa5ca50cc7e467f9be42bdcfe5655b3
lbruder <lbruder@0c85f470-1217-11de-aba5-9925624bc3b7>
2012-11-09 15:02:43 +0000

    Fisher LDA can now calculate covariance matricies (test needed)
    OutputStreamCallback changed
    
    git-svn-id: file:///mnt/svn/libmusic/trunk@923 0c85f470-1217-11de-aba5-9925624bc3b7

--- src/music/classification/lda_fisher.cpp
+++ src/music/classification/lda_fisher.cpp
@@ -4,7 +4,7 @@ namespace music
 {
     bool FisherLDAClassifier::learnModel(const std::vector<std::pair<Eigen::VectorXd, double> >& trainingData, ProgressCallbackCaller* callback)
     {
-        double steps = 3.0;
+        double steps = 2.0;
         
         if (callback != NULL)
             callback->progress(0.0, "initializing...");
@@ -15,6 +15,7 @@ namespace music
         if (callback != NULL)
             callback->progress(1.0/steps, "calculating means...");
         
+        Eigen::VectorXd tmpVec;
         
         //first: calculate means.
         int class1Count=0, class2Count=0;
@@ -40,6 +41,39 @@ namespace music
         
         if (callback != NULL)
             callback->progress(2.0/steps, "calculating covariance matrices...");
+        
+        //calculate covariance matricies
+        covariance1 = Eigen::MatrixXd(trainingData[0].first.size(), trainingData[0].first.size());
+        covariance1.setZero();
+        covariance2 = Eigen::MatrixXd(trainingData[0].first.size(), trainingData[0].first.size());
+        covariance2.setZero();
+        for (std::vector<std::pair<Eigen::VectorXd, double> >::const_iterator it = trainingData.begin(); it != trainingData.end(); it++)
+        {
+            if (it->second < 0.5)
+            {
+                //calculate covariance matrix
+                tmpVec = it->first - mean1;
+                covariance1 += tmpVec * tmpVec.transpose();
+            }
+            else
+            {
+                //calculate covariance matrix
+                tmpVec = it->first - mean2;
+                covariance2 += tmpVec * tmpVec.transpose();
+            }
+        }
+        //divide by number of elements in each class
+        covariance1 /= class1Count;
+        covariance2 /= class2Count;
+        
+        
+        if (callback != NULL)
+            callback->progress(3.0/steps, "calculating optimal discriminant...");
+        
+        
+        if (callback != NULL)
+            callback->progress(1.0, "finished.");
+        
         
         return true;
     }
--- src/music/classification/lda_fisher.hpp
+++ src/music/classification/lda_fisher.hpp
@@ -12,12 +12,17 @@ namespace music
     protected:
         Eigen::VectorXd mean1;
         Eigen::VectorXd mean2;
+        
+        Eigen::MatrixXd covariance1;
+        Eigen::MatrixXd covariance2;
     public:
         bool learnModel(const std::vector<std::pair<Eigen::VectorXd, double> >& trainingData, ProgressCallbackCaller* callback = NULL);
         double classifyVector(const Eigen::VectorXd& vector);
         
         const Eigen::VectorXd& getClass1Mean() {return mean1;}
         const Eigen::VectorXd& getClass2Mean() {return mean2;}
+        const Eigen::MatrixXd& getClass1Covariance() {return covariance1;}
+        const Eigen::MatrixXd& getClass2Covariance() {return covariance2;}
     };
 }
 
