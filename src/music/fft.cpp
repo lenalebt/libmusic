cbde7310c110125d0a28e0ee9a083320beb37495
lbruder <lbruder@0c85f470-1217-11de-aba5-9925624bc3b7>
2012-11-09 14:49:27 +0000

    fft is now working. need to adjust the tests, but for a first test, it's okay.
    git-svn-id: file:///mnt/svn/libmusic/trunk@782 0c85f470-1217-11de-aba5-9925624bc3b7

--- src/music/fft.cpp
+++ src/music/fft.cpp
@@ -1,1 +1,30 @@
 #include "fft.hpp"
+
+namespace music
+{
+    void FFT::doFFT(const kiss_fft_scalar *timeData, int timeLength, kiss_fft_cpx *freqData, int& freqLength)
+    {
+        //init kissfft
+        kiss_fftr_cfg cfg = kiss_fftr_alloc(timeLength, 0, NULL, NULL);
+        
+        //start kissfft
+        kiss_fftr(cfg, timeData, freqData);
+        
+        freqLength = timeLength/2 +1;
+        
+        free(cfg);
+    }
+    
+    void FFT::doiFFT(const kiss_fft_cpx *freqData, int freqLength, kiss_fft_scalar *timeData, int& timeLength)
+    {
+        //init kissfft
+        kiss_fftr_cfg cfg = kiss_fftr_alloc(freqLength, 1, NULL, NULL);
+        
+        //start kissfft
+        kiss_fftri(cfg, freqData, timeData);
+        
+        timeLength = 2*(freqLength-1);
+        
+        free(cfg);
+    }
+}
