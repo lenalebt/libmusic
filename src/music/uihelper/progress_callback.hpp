ca470247e7a7e0b68c332f62e4150db211e0d327
lbruder <lbruder@0c85f470-1217-11de-aba5-9925624bc3b7>
2012-11-09 15:01:03 +0000

    added a progress callback object that helps give the user feedback about the programs state.
    git-svn-id: file:///mnt/svn/libmusic/trunk@905 0c85f470-1217-11de-aba5-9925624bc3b7

---
+++ src/music/uihelper/progress_callback.hpp
@@ -0,0 +1,81 @@
+#ifndef PROGRESS_CALLBACK_HPP
+#define PROGRESS_CALLBACK_HPP
+
+#include <string>
+
+/**
+ * @brief This class is a progress callback functor.
+ * 
+ * This class is used to signal progress to the calling part of a function,
+ * even if the function is not finished yet. This can be used to inform the user
+ * of progress, or similar things. The function needs to support this - 
+ * functions that are known to have a looong runtime should support this.
+ * 
+ * @author Lena Brueder
+ * @date 2012-08-13
+ */
+class ProgressCallback
+{
+private:
+    
+protected:
+    
+public:
+    /**
+     * @brief This function will be called when some progress is made.
+     * 
+     * @param id the ID of the function that makes progress.
+     * @param percent the amount of the job that is finished (0.0 <= percent <= 1.0), if applicable.
+     *      if not applicable, percent < 0.0.
+     * @param progressMessage an optional message that might be presented to the user.
+     */
+    virtual void progress(const std::string& id, double percent, const std::string& progressMessage)=0;
+};
+
+/**
+ * @brief This class is a progress callback functor that encapsulates the id.
+ * 
+ * This class adds its ID to the call of progress() from ProgressCallback. Usage:
+ * @code
+ * //assume that this object is a ProgressCallback object.
+ * someOtherObject.doSomething(ProgressCallbackCaller(*this, "myName"));
+ * //someOtherObject will now call this->progress() from time to time, with the string "myName" as ID.
+ * @endcode
+ * 
+ * @author Lena Brueder
+ * @date 2012-08-13
+ * @see ProgressCallback
+ */
+class ProgressCallbackCaller
+{
+private:
+    std::string id;
+    ProgressCallback& callback;
+protected:
+    
+public:
+    /**
+     * @brief Creates a new ProgressCallbackCaller object.
+     * 
+     * @param callback A reference to a callback object
+     * @param id The id of the object. This is a name and may be freely chosen by the user. It will be used as id in ProgressCallbacks progress() function.
+     */
+    ProgressCallbackCaller(ProgressCallback& callback, const std::string& id = "") :
+        id(id), callback(callback)
+    {
+        
+    }
+    /**
+     * @brief Reports progress to an instance of ProgressCallback.
+     * @see ProgressCallback
+     * 
+     * @param percent the amount of the job that is finished (0.0 <= percent <= 1.0), if applicable.
+     *      if not applicable, percent < 0.0.
+     * @param progressMessage an optional message that might be presented to the user.
+     */
+    void progress(double percent, const std::string& progressMessage)
+    {
+        callback.progress(id, percent, progressMessage);
+    }
+};
+#endif //PROGRESS_CALLBACK_HPP
