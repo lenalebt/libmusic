9d3dc6233bc6ba9bd7f4c2f87199510f2564df66
lbruder <lbruder@0c85f470-1217-11de-aba5-9925624bc3b7>
2012-11-09 15:19:19 +0000

    added some classes for classification purposes. not tested, no contents yet.
    git-svn-id: file:///mnt/svn/libmusic/trunk@1030 0c85f470-1217-11de-aba5-9925624bc3b7

---
+++ src/music/classification/category.hpp
@@ -0,0 +1,15 @@
+#ifndef CLASSIFICATION_CATEGORY_HPP
+#define CLASSIFICATION_CATEGORY_HPP
+
+namespace music
+{
+    class Category
+    {
+    private:
+        
+    public:
+        
+    }
+}
+
+#endif //CLASSIFICATION_CATEGORY_HPP
---
+++ src/music/classification/classificationprocessor.hpp
@@ -0,0 +1,30 @@
+#ifndef CLASSIFICATIONPROCESSOR_HPP
+#define CLASSIFICATIONPROCESSOR_HPP
+
+#include "databaseentities.hpp"
+
+namespace music
+{
+
+    //TODO: add callbacks! these functions will be sloooow.
+    class ClassificationProcessor
+    {
+    private:
+        
+    public:
+        //bool recalculateCategories();
+        //recalculate the timbre model for the category (maybe other things). rebuild the classifier/train it
+        bool recalculateCategory(const databaseentities::Category& category, bool recalculateCategoryMembershipScores_ = true);
+        
+        //first delete all scores, then it is possible to read from the db with another thread all the new scores. or from the callback, then no other thread is necessary.
+        //recalulates all scores for songs to this category.
+        bool recalculateCategoryMembershipScores(const databaseentities::Category& category);
+        //recalculates the score for a single song (maybe better for debugging etc and may be used from the other function)
+        bool recalculateCategoryMembershipScore(const databaseentities::Category& category, const databaseentities::Recording& recording);
+        //called when a song is added to the db, most likely from the file preprocessor. will calculate all scores for the song for existing categories.
+        bool addRecording(const databaseentities::Recording&);
+        //calls more or less the appropriate database function
+        bool setRecordingCategoryExampleScore(const databaseentities::Recording& recording, const databaseentitie::Category& category, double score, bool recalculateCategory_ = true);
+    };
+}
+#endif //CLASSIFICATIONPROCESSOR_HPP
